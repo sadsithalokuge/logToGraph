6bc1138ba5b9d586120055e0b3ce19c35e25a100
Popiette <pier.jacquot@gmail.com>
2019-02-07 10:31:50 +0100

    Classes filtres (sélections de lignes)

---
+++ Filtre.cpp
@@ -0,0 +1,58 @@
+/*******************************************************************************************
+Créé le : 06 février 2019
+par : pierre
+e-mail :
+*******************************************************************************************/
+
+//------------- Realisation de la classe <Filtre> ( fichier Filtre.cpp ) -----------------
+
+//------------- Include système ------------------------------------------------------------
+#include <cstring>
+
+using namespace std;
+
+//------------- Include personnel ----------------------------------------------------------
+#include "Filtre.h"
+
+//------------- Constantes -----------------------------------------------------------------
+
+
+//------------- Methodes publiques ---------------------------------------------------------
+
+bool FiltreHeure::LigneEstConforme(const vector<string> infos) const
+{
+	int h = atoi(infos[0].substr(12, 2).c_str());
+	return (h == Heure);	
+}
+
+//------------- Surcharge d'opérateurs -----------------------------------------------------
+
+//------------- Constructeurs - Destructeur ------------------------------------------------
+Filtre::Filtre()
+{ 
+#ifdef MAP
+	cout << "Appel du constructeur de <Filtre>." << endl;
+#endif
+}
+
+FiltreHeure::FiltreHeure(int heure) : Filtre(), Heure(heure)
+{
+#ifdef MAP
+	cout << "Appel du constructeur de <FiltreHeure>." << endl;
+#endif
+}
+
+Filtre::~Filtre()
+{
+#ifdef MAP
+	cout << "Appel du destructeur de <Filtre>." << endl;
+#endif 
+}
+
+FiltreHeure::~FiltreHeure()
+{
+#ifdef MAP
+	cout << "Appel du destructeur de <FiltreHeure>." << endl;
+#endif
+}
+//------------- Methodes protégées ---------------------------------------------------------
---
+++ Filtre.h
@@ -0,0 +1,58 @@
+/******************************************************************
+Créé le : 06 février 2019
+par : pierre
+e-mail :
+******************************************************************/
+
+//-------------- Interface de la classe <Filtre> (fichier Filtre.h) ---------------------
+#if ! defined ( FILTRE_H )
+#define FILTRE_H
+
+//-------------- Interfaces utilisées -----------------------------
+#include <string>
+#include <vector>
+//-------------- Constantes ---------------------------------------
+//-------------- Types --------------------------------------------
+
+//-----------------------------------------------------------------
+//Rôle de la classe <Filtre>:
+//
+//-----------------------------------------------------------------
+
+class Filtre
+{
+	public:
+		//----- Methodes publiques -----
+		virtual bool LigneEstConforme(const std::vector<std::string> infos) const = 0;
+		//Mode d'emploi :
+		//	Retourne vrai si les infos de la ligne passées en paramètres sont
+		//	correct du point de vue du filtre.
+
+
+		//---- Surchage d'opérateurs ----
+		//--- Constructeurs - Destructeurs ---
+		Filtre();
+		virtual ~Filtre();
+
+	protected:
+		//----- Methodes protégées -----
+		//----- Attributs protégés -----
+
+};
+//------------ Autres définitions dépendantes de Filtre ----------
+
+class FiltreHeure : public Filtre
+{
+	public:
+		//----- Methodes publiques -----
+		virtual bool LigneEstConforme(const std::vector<std::string> infos) const;
+		
+		// --- Constructeurs - Destructeurs ---
+		FiltreHeure(int heure);
+		virtual ~FiltreHeure();
+	
+	private:
+		int Heure;
+};
+
+#endif // ! defined ( FILTRE_H )
