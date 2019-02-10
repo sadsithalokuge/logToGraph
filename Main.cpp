f36415c43c2608a8ff1215522a010e3fbc3be2df
Popiette <pier.jacquot@gmail.com>
2019-02-10 15:43:31 +0100

    Refactoring

--- Main.cpp
+++ Main.cpp
@@ -48,17 +48,11 @@ int main ( int argc , char * argv [ ] )
 	int heure;
 	list<Filtre *> * filtres = nullptr;
 
-	if ( argc == 1 )
-	{
-		afficherSyntaxe();
-		return 0;
-	}
-
 	int i = 1;
 	while ( i < argc )
 	{
 		string arg ( argv [ i ] );
-		if ( arg == "-g" && i < argc)
+		if (arg == "-g")
 		{
 			nomGraph = argv [ ++i ];
 		}
@@ -71,23 +65,20 @@ int main ( int argc , char * argv [ ] )
 
 			cout << "Exclusion des images et fichiers web." << endl;
 		}
-		else if ( arg == "-t")
+		else if (arg == "-t")
 		{
-			if ( i < argc )
+			try
 			{
-				try
-				{
-					heure = stoi ( argv [ ++i ] );
-					Filtre * f = new FiltreHeure(heure);
-					filtres = insererFiltre(filtres, f);
-				}
-				catch ( std::invalid_argument& e )
-				{
-					cout << "ERREUR : L'heure est mal formatée." << endl;
-					cout << "Merci de prendre connaissance de la syntaxe :" << endl;
-					afficherSyntaxe();
-					return 0;	
-				}
+				heure = stoi ( argv [ ++i ] );
+				Filtre * f = new FiltreHeure(heure);
+				filtres = insererFiltre(filtres, f);
+			}
+			catch ( std::invalid_argument& e )
+			{
+				cerr << "ERREUR : " << argv[i] << " n'a pas le format exigé pour une heure." << endl;
+				cerr << "Merci de prendre connaissance de la syntaxe :" << endl;
+				afficherSyntaxe();
+				return 0;	
 			}
 		}
 		else
@@ -95,7 +86,7 @@ int main ( int argc , char * argv [ ] )
 			fichierLog = argv[i];
 			if ( fichierLog.find ( ".log" ) == string::npos )
 			{
-				cout << "ATTENTION : Le fichier spécifié n'est pas au format .log" << endl
+				cerr << "ATTENTION : Le fichier spécifié n'est pas au format .log" << endl
 					<< "Il est possible que le programme ne fonctionne pas correctement." << endl;
 			}
 		}
@@ -104,7 +95,15 @@ int main ( int argc , char * argv [ ] )
 	}
 
 	if(fichierLog != "")
+	{
 		lireLog(fichierLog, filtres, nomGraph);
+	}
+	else
+	{
+		cerr << "Aucun fichier de log fournis. Consultez la syntaxe :" << endl;
+		afficherSyntaxe();
+	}
+	
 	return 0;
 } //----- fin de Main
 
@@ -133,10 +132,10 @@ void lireLog(string nomFichier, list<Filtre *> * filtres, string nomGraphe)
 
 void afficherSyntaxe()
 {
-	cout << "L'execution de ce programme nécessite des arguments" << endl;
+	cout << "LogToGraph, TP C++ n°4." << endl;
 	cout << "Voici les synthaxes possibles :" << endl;
-	cout << "./main nomLog.log" << endl;
-	cout << "./main -t xx nomLog.log (avec xx entre 00 et 23)" << endl;
-	cout << "./main -g nomGraph.dot nomLog.log" << endl;
-	cout << "./main -e nomLog.log" << endl;
+	cout << "./analog nomLog.log" << endl;
+	cout << "./analog -t xx nomLog.log (avec xx entre 00 et 23)" << endl;
+	cout << "./analog -g nomGraph.dot nomLog.log" << endl;
+	cout << "./analog -e nomLog.log" << endl;
 }
